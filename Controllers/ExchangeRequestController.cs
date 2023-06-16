5e87d2bcb6842bc293f2fb1954a74afee7a530f5
Nguyen Trung Tin <trungtin9620@gmail.com>
2023-06-16 23:57:03 +0700

    create-request exchange

---
+++ Controllers/ExchangeRequestController.cs
@@ -0,0 +1,56 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SecondhandStore.EntityRequest;
+using SecondhandStore.Models;
+using SecondhandStore.Services;
+
+namespace SecondhandStore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExchangeRequestController : ControllerBase
+    {
+        private readonly ExchangeRequestService _exchangeRequestService;
+        private readonly IMapper _mapper;
+        public ExchangeRequestController(ExchangeRequestService exchangeRequestService, IMapper mapper)
+        {
+            _exchangeRequestService = exchangeRequestService;
+            _mapper = mapper;
+        }
+
+        // GET all action
+        [HttpGet]
+        public async Task<IActionResult> GetRequestList()
+        {
+            var requestList = await _exchangeRequestService.GetAllRequest();
+
+            if (requestList.Count == 0 || !requestList.Any())
+                return NotFound();
+
+            return Ok(requestList);
+        }
+
+        // GET by Id action
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetTopUpById(int id)
+        {
+            var existingRequest = await _exchangeRequestService.GetRequestById(id);
+            if (existingRequest is null)
+                return NotFound();
+            return Ok(existingRequest);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNewTopUp(ExchangeRequestCreateRequest exchangeRequestCreateRequest)
+        {
+            var mappedRequest = _mapper.Map<ExchangeRequest>(exchangeRequestCreateRequest);
+
+            await _exchangeRequestService.AddRequest(mappedRequest);
+
+            return CreatedAtAction(nameof(GetRequestList),
+                new { id = mappedRequest.RequestDetailId },
+                mappedRequest);
+        }
+    }
+}
