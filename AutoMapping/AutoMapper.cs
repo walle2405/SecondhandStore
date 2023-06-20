44b1951c332898aee40eb7e8e93fa0871cde7b6a
tinnguyentrung962k <trungtin9620@gmail.com>
2023-06-20 11:17:45 +0700

    adding exchange order

--- AutoMapping/AutoMapper.cs
+++ AutoMapping/AutoMapper.cs
@@ -12,6 +12,8 @@ public class AutoMapper : Profile
         MapRole();
         MapAccount();
         MapTopUp();
+        MapExchangeRequest();
+        MapExchangeOrder();
     }
 
     private void MapRole()
@@ -59,5 +61,21 @@ public class AutoMapper : Profile
             .ReverseMap();
         CreateMap<TopUpCreateRequest, TopUp>()
            .ReverseMap();
+    }
+    public void MapExchangeRequest() { 
+        CreateMap<ExchangeRequest,ExchangeRequestCreateRequest>()
+            .ReverseMap();
+        CreateMap<ExchangeRequestCreateRequest, ExchangeRequest>()
+            .ReverseMap();
+    }
+    private void MapExchangeOrder() {
+        CreateMap<ExchangeOrder,ExchangeOrderCreateRequest>()
+            .ReverseMap();
+        CreateMap<ExchangeOrderCreateRequest,ExchangeOrder>()
+            .ReverseMap();
+        CreateMap<ExchangeOrder, ExchangeOrderUpdateRequest>()
+            .ReverseMap();
+        CreateMap <ExchangeOrderUpdateRequest, ExchangeOrder>()
+            .ReverseMap();
     }
 }
