458a4b2833f5cab82050f2d328855943667daa87
Bui Hoang Hai <buihoanghai2405@gmail.com>
2023-07-01 12:01:31 +0700

    new tables

---
+++ Models/PostStatus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SecondhandStore.Models
+{
+    public partial class PostStatus
+    {
+        public PostStatus()
+        {
+            ExchangeOrders = new HashSet<ExchangeOrder>();
+            Posts = new HashSet<Post>();
+        }
+
+        public int PostStatusId { get; set; }
+        public string PostStatusName { get; set; }
+
+        public virtual ICollection<ExchangeOrder> ExchangeOrders { get; set; }
+        public virtual ICollection<Post> Posts { get; set; }
+    }
+}
