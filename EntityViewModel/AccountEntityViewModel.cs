7b5b88f69cdd9de67cfc7850cee5e6c4d348e908
bug1422 <binlambaobao142@gmail.com>
2023-07-26 10:52:56 +0700

    add dob to account entity view model

--- EntityViewModel/AccountEntityViewModel.cs
+++ EntityViewModel/AccountEntityViewModel.cs
@@ -10,6 +10,7 @@ public class AccountEntityViewModel
     public string RoleId { get; set; }
     public string Address { get; set; }
     public string PhoneNo { get; set; }
+    public DateTime Dob { get; set; }
     public int pointBalance { get; set; }
     public int credibilityPoint { get; set; }
     public bool IsActive { get; set; }
