99c5f2383e5e48bde6b825a169f870913dd6e3ba
walle <buihoanghai2405@gmail.com>
2023-07-26 14:35:09 +0700

    add credibilityPoint to AccountEntityViewModel.cs

--- EntityViewModel/AccountEntityViewModel.cs
+++ EntityViewModel/AccountEntityViewModel.cs
@@ -7,9 +7,11 @@ public class AccountEntityViewModel
     public int accountId { get; set; }
     public string fullName { get; set; }
     public string email { get; set; }
+    public DateTime dob { get; set; }
     public string roleId { get; set; }
     public string address { get; set; }
     public string phoneNo { get; set; }
     public int pointBalance { get; set; }
+    public int credibilityPoint { get; set; }
     public bool isActive { get; set; }
 }
